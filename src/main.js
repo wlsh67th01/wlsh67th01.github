5912a17a5460c67142db8940d53b3663e5604aa0
wlsh67th01 <168455840+wlsh67th01@users.noreply.github.com>
2024-05-24 07:40:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import navbar from "./share/Navbar.vue";
+import VueFeather from "vue-feather";
 
-
+/*
 createApp(App)
     .use(router)
-    .mount('#app')
+    .mount('#app')*/
+
+createApp(App)
+    .use(router, VueFeather)
+    .component("navbar", navbar)
+    .component(VueFeather.name, VueFeather)
+    .mount("#app");
 
 
 
