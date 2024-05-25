f166606cc3f026e81770bb2f263aeb4be82db28b
wlsh67th01 <168455840+wlsh67th01@users.noreply.github.com>
2024-05-25 13:47:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': {},
-  },
-  plugins: [vue()],
-  build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
-});
+  base: '/wlsh67th01.github.io/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
